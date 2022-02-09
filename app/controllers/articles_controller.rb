a9fef8cc71edef70a12a347d5ef8f44ccd672a56
Prabhanshu <prabhanshu.itworks@gmail.com>
2022-02-09 19:03:40 +0530

    DRY changes done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
         # byebug
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
     end
 
     def index
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
     end
 
     
@@ -22,7 +24,8 @@ class ArticlesController < ApplicationController
 
     def create
         # render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        # @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         # render plain: @article
         
         if @article.save
@@ -35,13 +38,32 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        # @article = Article.find(params[:id])
+        # if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article updated successfully"
             redirect_to @article
         else
             render 'edit'
         end
+    end
+
+    def destroy
+        # @article = Article.find(params[:id])
+
+        @article.destroy
+        redirect_to articles_path
+    end
+
+
+    private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    
+    def article_params
+        params.require(:article).permit(:title, :description) 
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # root 'application#hello'
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
+
 end
