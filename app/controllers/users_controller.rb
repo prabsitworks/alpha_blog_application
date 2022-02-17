fa14595146e63ea9cc9c45bd679e68796ffedf7f
Prabhanshu <prabhanshu.itworks@gmail.com>
2022-02-17 12:22:23 +0530

    pagination added in articles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,16 @@ class UsersController < ApplicationController
     
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
     def new
         @user = User.new
+    end
+
+    def index
+        # @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def edit
@@ -17,7 +22,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if@user.update(user_params)
             flash[:notice] = "Your account infromation was successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
